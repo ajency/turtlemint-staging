a1b1ec375c70118ef60e72a4fa92db91a4d195e0
Mamata <mamata@ajency.in>
2023-02-23 19:44:59 +0530

    Uncommented GTM Code from headers

--- wp-content/themes/turtlemint/header-tmhomereskin.php
+++ wp-content/themes/turtlemint/header-tmhomereskin.php
@@ -94,26 +94,16 @@
 			<link rel="preload" href="<?php bloginfo('stylesheet_directory'); ?>/tm-assets/css/tm-styles.css?t=<?php echo time(); ?>" as="style" onload="this.onload=null;this.rel='stylesheet'">
 			<noscript><link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/tm-assets/css/tm-styles.css?t=<?php echo time(); ?>"></noscript>
 			<!-- defered css end -->
-
-			<!-- Google Tag Manager -->
-			<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-				new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-				j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-				'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f)
-				;
-				})(window,document,'script','dataLayer','GTM-PCZGBP');
-			</script>
-			<!-- End Google Tag Manager -->
 		<?php } ?>
 		
 		<!-- Google Tag Manager -->
-		<!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f)
 			;
 			})(window,document,'script','dataLayer','GTM-PCZGBP');
-		</script> -->
+		</script>
 		<!-- End Google Tag Manager -->
 		<!-- Google tag (gtag.js) -->
 		<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-61873031-16"></script>
--- wp-content/themes/turtlemint/header-tmproductreskin.php
+++ wp-content/themes/turtlemint/header-tmproductreskin.php
@@ -762,13 +762,13 @@
 		<script src="<?php bloginfo('stylesheet_directory'); ?>/tm-assets/mn/scripts/vendor/modernizr-min.js"></script>
 		<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory'); ?>/tm-assets/css/mobilemenu-reskin.css"/>
 		<!-- Google Tag Manager -->
-		<!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f)
 			;
 			})(window,document,'script','dataLayer','GTM-PCZGBP');
-		</script> -->
+		</script>
 		<!-- End Google Tag Manager -->
 		<!-- Google tag (gtag.js) -->
 		<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-61873031-16"></script> -->
